98495f105c2faf4f2201678e5677cc08df9ccfb0
AntonioRamos17 <alu0101480367@ull.edu.es>
2022-04-05 17:59:23 +0000

    finalizado

--- polynomial.h
+++ polynomial.h
@@ -275,13 +275,21 @@ Polynomial Polynomial::SumaPolinomios(const Polynomial& pol) {
   Polynomial result;
   if (get_size() >= pol.get_size()) {
     result.resize(get_size());
-    for (int i = 0; i < pol.get_size(); ++i) {
+    int i{0};
+    for (i = 0; i < pol.get_size(); ++i) {
       result.at(i) = at(i) + pol.at(i);
+    }
+    for (int k = i; k < get_size(); ++k) {
+      result.at(k) = at(k);
     }
   } else if (pol.get_size() > get_size()) {
     result.resize(pol.get_size());
-    for (int j = 0; j < get_size(); ++j) {
+    int j{0};
+    for ( j = 0; j < get_size(); ++j) {
       result.at(j) = at(j) + pol.at(j);
+    }
+    for (int z = j; z < get_size(); ++z) {
+      result.at(z) = pol.at(z);
     }
   }
   return result;
@@ -324,13 +332,21 @@ Polynomial Polynomial::RestaPolinomios(const Polynomial& pol) {
   Polynomial result;
   if (get_size() >= pol.get_size()) {
     result.resize(get_size());
-    for (int i = 0; i < pol.get_size(); ++i) {
+    int i{0};
+    for (i = 0; i < pol.get_size(); ++i) {
       result.at(i) = at(i) - pol.at(i);
+    }
+    for (int k = i; k < get_size(); ++k) {
+      result.at(k) = at(k);
     }
   } else if (pol.get_size() > get_size()) {
     result.resize(pol.get_size());
-    for (int j = 0; j < get_size(); ++j) {
+    int j{0};
+    for ( j = 0; j < get_size(); ++j) {
       result.at(j) = at(j) - pol.at(j);
+    }
+    for (int z = j; z < get_size(); ++z) {
+      result.at(z) = pol.at(z);
     }
   }
   return result;
